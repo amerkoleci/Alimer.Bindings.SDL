fb9ea5fe36d2795efd01989e469905edc3539e2a
Amer Koleci <amerkoleci@gmail.com>
2024-06-18 11:11:18 +0200

    Update SDL to https://github.com/libsdl-org/SDL/commit/ea8df4657535812c9f7ebdfd066d26ac118718e9

--- build/cmake_vs2022_UWP.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cmake -B "vs2022_uwp" -S "./../" -G "Visual Studio 17 2022" -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 %*
-echo If cmake failed then be sure to check the "Universal Windows Platform development" checkbox in the Visual Studio Installer
-echo Open vs2022_uwp\Alimer.Native.SDL.sln to build the project.
